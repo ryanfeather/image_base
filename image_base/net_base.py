3bd673159fdafd067c8aa8517f00970ed6279bd1
ryanfeather <Boomer11>
2017-04-10 18:40:27 -0400

    small vgg arch

--- image_base/net_base.py
+++ image_base/net_base.py
@@ -31,34 +31,34 @@ def make_small_vgg19(inputs, dim1=64):
     :param dim1:
     :return:
     """
-    x = Conv2D(dim1, 3, 3, activation='relu', padding='same', name='block1_conv1')(inputs)
-    x = Conv2D(dim1, 3, 3, activation='relu', padding='same', name='block1_conv2')(x)
+    x = Conv2D(dim1, 3, 3, activation='relu', border_mode='same', name='block1_conv1')(inputs)
+    x = Conv2D(dim1, 3, 3, activation='relu', border_mode='same', name='block1_conv2')(x)
     x = MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool')(x)
 
     # Block 2
-    x = Conv2D(dim1*2, 3, 3, activation='relu', padding='same', name='block2_conv1')(x)
-    x = Conv2D(dim1*2, 3,3, activation='relu', padding='same', name='block2_conv2')(x)
+    x = Conv2D(dim1*2, 3, 3, activation='relu', border_mode='same', name='block2_conv1')(x)
+    x = Conv2D(dim1*2, 3,3, activation='relu', border_mode='same', name='block2_conv2')(x)
     x = MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool')(x)
 
     # Block 3
-    x = Conv2D(dim1*4, 3,3, activation='relu', padding='same', name='block3_conv1')(x)
-    x = Conv2D(dim1*4, 3,3, activation='relu', padding='same', name='block3_conv2')(x)
-    x = Conv2D(dim1*4, 3,3, activation='relu', padding='same', name='block3_conv3')(x)
-    x = Conv2D(dim1*4, 3,3, activation='relu', padding='same', name='block3_conv4')(x)
+    x = Conv2D(dim1*4, 3,3, activation='relu', border_mode='same', name='block3_conv1')(x)
+    x = Conv2D(dim1*4, 3,3, activation='relu', border_mode='same', name='block3_conv2')(x)
+    x = Conv2D(dim1*4, 3,3, activation='relu', border_mode='same', name='block3_conv3')(x)
+    x = Conv2D(dim1*4, 3,3, activation='relu', border_mode='same', name='block3_conv4')(x)
     x = MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool')(x)
 
     # Block 4
-    x = Conv2D(dim1*8, 3,3, activation='relu', padding='same', name='block4_conv1')(x)
-    x = Conv2D(dim1*8, 3,3, activation='relu', padding='same', name='block4_conv2')(x)
-    x = Conv2D(dim1*8, 3,3, activation='relu', padding='same', name='block4_conv3')(x)
-    x = Conv2D(dim1*8, 3,3, activation='relu', padding='same', name='block4_conv4')(x)
+    x = Conv2D(dim1*8, 3,3, activation='relu', border_mode='same', name='block4_conv1')(x)
+    x = Conv2D(dim1*8, 3,3, activation='relu', border_mode='same', name='block4_conv2')(x)
+    x = Conv2D(dim1*8, 3,3, activation='relu', border_mode='same', name='block4_conv3')(x)
+    x = Conv2D(dim1*8, 3,3, activation='relu', border_mode='same', name='block4_conv4')(x)
     x = MaxPooling2D((2, 2), strides=(2, 2), name='block4_pool')(x)
 
     # Block 5
-    x = Conv2D(dim1*8, 3,3, activation='relu', padding='same', name='block5_conv1')(x)
-    x = Conv2D(dim1*8, 3,3, activation='relu', padding='same', name='block5_conv2')(x)
-    x = Conv2D(dim1*8, 3,3, activation='relu', padding='same', name='block5_conv3')(x)
-    x = Conv2D(dim1*8, 3,3, activation='relu', padding='same', name='block5_conv4')(x)
+    x = Conv2D(dim1*8, 3,3, activation='relu', border_mode='same', name='block5_conv1')(x)
+    x = Conv2D(dim1*8, 3,3, activation='relu', border_mode='same', name='block5_conv2')(x)
+    x = Conv2D(dim1*8, 3,3, activation='relu', border_mode='same', name='block5_conv3')(x)
+    x = Conv2D(dim1*8, 3,3, activation='relu', border_mode='same', name='block5_conv4')(x)
     x = MaxPooling2D((2, 2), strides=(2, 2), name='block5_pool')(x)
     model = Model(inputs, x, name='vgg19')
     return model
