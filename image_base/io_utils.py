f68b0d2b66615f24066e7b031e7d3626cffb2560
ryanfeather <Boomer11>
2017-05-02 21:48:54 -0400

    added more scaling options

--- image_base/io_utils.py
+++ image_base/io_utils.py
@@ -81,7 +81,7 @@ def get_im_cv2(path, resolution=None, ratio=1,size=None, upscale=True, downscale
             width = int(resolution*ratio)
             size = (width, resolution)
         else:
-            width = resolution
+            width = size[0]
         do_resize = (upscale and (width > img.shape[0] or resolution > img.shape[1]) or
                      downscale and (width < img.shape[0] or resolution < img.shape[1]))
         if do_resize:
